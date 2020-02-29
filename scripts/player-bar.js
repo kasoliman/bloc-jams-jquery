e8fdb057f50773de140ba00f4da053430b47e8de
Karim Soliman <ka.soliman@gmail.com>
2020-02-29 12:42:49 +0300

    Assignment 17: Completed previous track button

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -4,14 +4,30 @@
     $(this).attr('playstate', player.playState);
   });
 
+
+//Click handler for next track button
 $('button#next').on('click', function(){
   if(player.playState !== 'playing') {return; }
 
   const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
   const nextSongIndex = currentSongIndex + 1;
   if (nextSongIndex >= album.songs.length) { return; }
-  
+
   const nextSong = album.songs[nextSongIndex];
   player.playPause(nextSong);
 });
+
+
+//Click handler for previous track button
+$('button#previous').on('click', function(){
+  if(player.playState !== 'playing') {return; }
+
+  const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+  const previousSongIndex = currentSongIndex - 1;
+  if (previousSongIndex < 0) {return; }
+
+  const previousSong = album.songs[previousSongIndex];
+  player.playPause(previousSong);
+});
+
 }
